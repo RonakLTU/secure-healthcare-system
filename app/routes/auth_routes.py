f77a4688aae6961e55bdb40d09fb9e75c71bcdd4
RonakLTU <2511538@leedstrinity.ac.uk>
2026-03-11 16:43:36 +0000

    Added input validation

--- app/routes/auth_routes.py
+++ app/routes/auth_routes.py
@@ -1,5 +1,6 @@
-from flask import request, render_template
+from flask import request, render_template, redirect
 import sqlite3
+import re
 from app.security.password_utils import hash_password
 
 DATABASE = "database/auth.db"
@@ -12,21 +13,47 @@ def register_user():
         name = request.form["name"]
         email = request.form["email"]
         password = request.form["password"]
-        role = "patient"
+        role = request.form["role"]
+
+        # EMAIL VALIDATION
+        email_pattern = r'^[\w\.-]+@[\w\.-]+\.\w+$'
+
+        if not re.match(email_pattern, email):
+            return "Invalid email format"
+
+        # PASSWORD VALIDATION
+        if len(password) < 8:
+            return "Password must be at least 8 characters"
+
+        if not re.search(r'[A-Z]', password):
+            return "Password must contain uppercase letter"
+
+        if not re.search(r'[a-z]', password):
+            return "Password must contain lowercase letter"
+
+        if not re.search(r'[0-9]', password):
+            return "Password must contain number"
+
+        if not re.search(r'[!@#$%^&*(),.?\":{}|<>]', password):
+            return "Password must contain special character"
 
         hashed_password = hash_password(password)
 
-        conn = sqlite3.connect(DATABASE)
-        cursor = conn.cursor()
+        try:
+            with sqlite3.connect(DATABASE) as conn:
 
-        cursor.execute(
-            "INSERT INTO users (name,email,password,role) VALUES (?,?,?,?)",
-            (name, email, hashed_password, role)
-        )
+                cursor = conn.cursor()
 
-        conn.commit()
-        conn.close()
+                cursor.execute(
+                    "INSERT INTO users (name,email,password,role) VALUES (?,?,?,?)",
+                    (name, email, hashed_password, role)
+                )
 
-        return "User Registered Successfully"
+                conn.commit()
+
+        except sqlite3.IntegrityError:
+            return "Email already exists"
+
+        return redirect("/login")
 
     return render_template("register.html")
